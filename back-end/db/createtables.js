34a8a6b53615169ff0bb197c293fc171c6a5f647
Seth <s.zim517@gmail.com>
2018-03-08 11:23:35 -0500

    Db

--- back-end/db/createtables.js
+++ back-end/db/createtables.js
@@ -10,8 +10,8 @@ User.sync({ force: true })
     .then(() => Relationship.sync({ force: true }))
     .then(() => Prompt_Topic.sync({ force: true }))
     .then(() => Prompt.sync({ force: true }))
-    .then(() => User_Entry.sync({ force: true }))
     .then(() => User_Book.sync({ force: true }))
+    .then(() => User_Entry.sync({ force: true }))
     .then(() => User_Book_Contributor.sync({ force: true }))
     .then(() => {
         Prompt.bulkCreate([
