a320b78dcec805eb123789cfc3bd5c92d5add235
Seth <s.zim517@gmail.com>
2018-03-21 11:30:15 -0400

    Gives user prompt when deleting book instead of instant delete onclick

--- front-end/src/components/Account/UserBookCover.js
+++ front-end/src/components/Account/UserBookCover.js
@@ -10,22 +10,35 @@ class UserBookCover extends Component {
 
         this.state = {
             bookOptionsView: false,
-            deleteHover: false
+            deleteHover: false,
+            deleteConfirm: false
         }
     }
 
-    _handleBookDelete = () => {
+    _handleBookDelete2 = () => {
         axios.post('/api/book/delete', {
             id: this.props.id
         })
-        .then(data => this.props.filterDeletedBooks(data.data.id))
+        .then(data => {
+            this.props.filterDeletedBooks(data.data.id);
+            this.setState({ deleteConfirm: false })
+        })
+    }
+
+    _handleBookDelete1 = (event) => {
+        this.setState({ deleteConfirm: true, bookOptionsView: true });
+        event.stopPropagation();
+    }
+
+    _handleCoverClick = (event) => {
+        this.setState({ bookOptionsView: !this.state.bookOptionsView });
     }
 
     render() {
         return (
-            <div onClick={() => this.setState({ bookOptionsView: !this.state.bookOptionsView })} className='bookCoverStyles'>
+            <div onClick={(event) => this._handleCoverClick(event)} className='bookCoverStyles'>
                 {this.state.bookOptionsView
-                    ? <div style={{ height: '90%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around' }}>
+                    ? <div className="bookOptionsView">
                         <Link to={`/book/prompts/${this.props.currentUser}/${this.props.id}`}>
                             <button>Add new entries</button>
                         </Link>
@@ -35,7 +48,9 @@ class UserBookCover extends Component {
                         <Link to={`/book/prompts/paid/${this.props.currentUser}/${this.props.id}`}>
                             <button>View my book</button>
                         </Link>
-                        <button className="delete-btn" onClick={() => this._handleBookDelete()}>Delete Book</button>
+                        {this.state.deleteConfirm
+                        ? <button className="delete-btn" onClick={() => this._handleBookDelete2()}>Are you sure?</button>
+                        : <button className="delete-btn" onClick={(event) => this._handleBookDelete1(event)}>Delete Book</button>}
                     </div>
                     : <p>{this.props.title}</p>}
                     
